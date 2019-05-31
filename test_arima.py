d1f2524e28dac491acfb5bb699aeb0afc515a2b9
italamanova <irusik020596@gmail.com>
2019-05-31 16:08:40 +0300

    SARIMAX

--- test_arima.py
+++ test_arima.py
@@ -3,16 +3,18 @@ import plotly.plotly as ply
 from plotly.plotly import plot_mpl
 from statsmodels.tsa.seasonal import seasonal_decompose
 from visualizer import plot
+import statsmodels.api as sm
 
 import pandas as pd
 
-data = pd.read_csv('./data/Electric_Production.csv', index_col=0)
+dataset = pd.read_csv('./data/Electric_Production.csv', index_col=0)
+data = dataset.loc['1985-01-01':'2018-01-01']
 print(data.head())
 
 data.index = pd.to_datetime(data.index)
 data.columns = ['Energy Production']
 
-plot(data, xlabel='DateTime', ylabel='Energy consumption')
+plot(data, ylabel='Energy consumption', title='Energy consumption')
 print('PM10 description', data['Energy Production'].describe())
 print(len(data))
 
@@ -20,11 +22,33 @@ print(len(data))
 # fig = result.plot()
 # # plot_mpl(fig)
 #
-stepwise_model = auto_arima(data, start_p=1, start_q=1,
-                            max_p=3, max_q=3, m=12,
-                            start_P=0, seasonal=True,
-                            d=1, D=1, trace=True,
-                            error_action='ignore',
-                            suppress_warnings=True,
-                            stepwise=True)
-print(stepwise_model.aic())
+# stepwise_model = auto_arima(data, start_p=1, start_q=1,
+#                             max_p=3, max_q=3, m=12,
+#                             start_P=0, seasonal=True,
+#                             d=1, D=1, trace=True,
+#                             error_action='ignore',
+#                             suppress_warnings=True,
+#                             stepwise=True)
+#
+# print(stepwise_model.aic())
+train = data.loc['1985-01-01':'2016-12-01']
+test = data.loc['2017-01-01':]
+
+stepwise_model = sm.tsa.statespace.SARIMAX(train, order=(1, 1, 1), seasonal_order=(2, 1, 2, 12))
+
+# print(stepwise_model.aic())
+model_fit = stepwise_model.fit()
+
+print(model_fit.summary())
+future_forecast = model_fit.predict('2017-01-01','2018-01-01')
+print(future_forecast)
+
+future_forecast = pd.DataFrame(future_forecast, index=test.index, columns=['Prediction'])
+plot(pd.concat([test, future_forecast], axis=1))
+
+'''
+Fit ARIMA: order=(1, 1, 1) seasonal_order=(2, 1, 2, 12); AIC=1855.121, BIC=1887.033, Fit time=17.693 seconds
+
+'''
+
+plot(pd.concat([data,future_forecast],axis=1))
--- visualizer.py
+++ visualizer.py
@@ -1,7 +1,8 @@
 import matplotlib.pyplot as plt
+import pandas as pd
 
 
-def plot(dataset, xlabel='Date time', ylabel='PM10', title='Time Series of PM10 by date time'):
+def plot(dataset, xlabel='DateTime', ylabel='Value', title='Plot'):
     dataset.plot()
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
@@ -9,10 +10,19 @@ def plot(dataset, xlabel='Date time', ylabel='PM10', title='Time Series of PM10 
     plt.show()
 
 
-def scatter_plot(dataset, xlabel='Date time', ylabel='PM10', title='Time Series of PM10 by date time',
+def scatter_plot(dataset, xlabel='DateTime', ylabel='Value', title='Plot',
                  point_size=10):
     plt.scatter(dataset.index, dataset, s=point_size)
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
     plt.title(title)
     plt.show()
+
+
+# data1 = pd.DataFrame([[1, 34],
+#                       [2, 30],
+#                       [3, 16]])
+# data2 = pd.DataFrame([[1, 1],
+#                       [2, 2],
+#                       [3, 3]])
+# plot(pd.concat([data1, data2], axis=1))
