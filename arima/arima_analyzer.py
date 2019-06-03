4bcbc9f8a4e90b6745b5f42dcb449cc1515fa540
italamanova <irusik020596@gmail.com>
2019-06-03 17:50:26 +0300

    plot_one_file added

--- arima/arima_analyzer.py
+++ arima/arima_analyzer.py
@@ -32,28 +32,38 @@ def plot_all_data(folder_path):
         print('%s description' % file, data[data.columns[0]].describe())
         print(len(data))
 
+def plot_one_file(file, start='2008-01-01', end='2018-03-09'):
+    dataset = pd.read_csv(file, index_col=0)
+    dataset.index = pd.to_datetime(dataset.index)
+    data = dataset[start:end]
 
-def plot_to_plotly(folder_path):
-    os.chdir(folder_path)
+    plot(data, ylabel=data.columns[0], title=file)
+    print('%s description' % file, data[data.columns[0]].describe())
+    print(len(data))
 
-    extension = 'csv'
-    all_filenames = [i for i in glob.glob('Centar_CO.{}'.format(extension))]
 
-    for file in all_filenames:
-        df = pd.read_csv(file, index_col=0)
-        print(df)
-        df.index = pd.to_datetime(df.index)
-        df = df.loc['2010-01-01':'2010-12-01']
-
-        trace = go.Scatter(x=[df.index], y=[df.columns[0]],
-                           name=file)
-        layout = go.Layout(title=file,
-                           plot_bgcolor='rgb(230, 230,230)',
-                           showlegend=True)
-        fig = go.Figure(data=[trace], layout=layout)
-
-        py.plot(fig, filename=file)
+# def plot_to_plotly(folder_path):
+#     os.chdir(folder_path)
+#
+#     extension = 'csv'
+#     all_filenames = [i for i in glob.glob('Centar_CO.{}'.format(extension))]
+#
+#     for file in all_filenames:
+#         df = pd.read_csv(file, index_col=0)
+#         print(df)
+#         df.index = pd.to_datetime(df.index)
+#         df = df.loc['2010-01-01':'2010-12-01']
+#
+#         trace = go.Scatter(x=[df.index], y=[df.columns[0]],
+#                            name=file)
+#         layout = go.Layout(title=file,
+#                            plot_bgcolor='rgb(230, 230,230)',
+#                            showlegend=True)
+#         fig = go.Figure(data=[trace], layout=layout)
+#
+#         py.plot(fig, filename=file)
 
 
 # plot_all_data('../pollution_data/new_data')
-plot_to_plotly('../pollution_data/new_data')
+plot_one_file('../pollution_data/new_data/Centar_CO.csv', start='2013-03-01', end='2013-03-02')
+# plot_to_plotly('../pollution_data/new_data')
