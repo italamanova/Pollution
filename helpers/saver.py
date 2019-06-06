7c8f83dc1a42b265651ce1c91ad6428852741743
italamanova <irusik020596@gmail.com>
2019-06-06 12:33:42 +0300

    saver added

---
+++ helpers/saver.py
@@ -0,0 +1,12 @@
+import os
+from pathlib import Path
+
+parent_dir_path = Path(__file__).parents[1]
+
+
+def get_autosave_path(file, params):
+    filename_w_ext = os.path.basename(file)
+    filename, file_extension = os.path.splitext(filename_w_ext)
+
+    new_file_name = '%s/pollution_data/cut_data/%s.csv' % (parent_dir_path, filename)
+    return new_file_name
