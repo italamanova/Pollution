f23cd53241876a0e245aecb1eb3092db1ba04d31
italamanova <irusik020596@gmail.com>
2019-06-27 18:56:22 +0300

    test box-cox

--- stationarity/stationarizer.py
+++ stationarity/stationarizer.py
@@ -1,3 +1,27 @@
-def make_stationary(df):
-    pass
-    
+class Stationarity:
+    def __init__(self, df):
+        self.df = df
+        self.box_cox_lambda = 1
+        self.diff = 1
+        self.sdiff = 1
+        self.criteria = ['adf', 'kpss']
+
+    def check_adf(self):
+        pass
+
+    def check_kpss(self):
+        pass
+
+    def make_box_cox(self):
+        pass
+
+    def make_diff(self):
+        pass
+
+    def make_sdiff(self):
+        pass
+
+    def stationarize(self):
+        df = self.df
+        
+
